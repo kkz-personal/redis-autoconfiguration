05b699ae37b271476101c2b06c269d9590b6ee52
kmz <maozhi.ke@okcoin.net>
2019-07-18 10:54:30 +0800

    Initial commit

---
+++ src/main/java/com/example/redisautoconfiguration/RedisAutoconfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.example.redisautoconfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisAutoconfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisAutoconfigurationApplication.class, args);
+	}
+
+}
